64bbff7698cfc5fe988fb86004e9fd379720b908
H. Watanabe <kaityo@users.sourceforge.jp>
2021-05-07 19:23:26 +0900

    adds makefile

--- convert/convert.rb
+++ convert/convert.rb
@@ -1,34 +1,21 @@
-kai = ["一","二","三","四","五","六","七","八","九"]
-kai_num = 0
-q_num = 0
-subq_num = 0
-
-while line = gets
-  if line=~/\{\\Large (.*)\}/
-    puts "# 第#{kai[kai_num]}回 #{$1}"
-    kai_num += 1
-    q_num = 0
-    subq_num = 0
-  elsif line=~/\\question(.*)/
-    q_num += 1
-    puts "## #{q_num}."
-    puts
-    puts "#{$1}"
-    subq_num = 0
-  elsif line=~/\\subquestion\{(.*)/
-    subq_num += 1
-    puts "### (#{subq_num})"
-    puts
-    puts "#{$1}"
-  elsif line=~/^}\r$/
-  elsif line=~/^{\\huge/
-  elsif line=~/\\begin{center}/
-  elsif line=~/\\end{center}/
-  elsif line=~/\\newpage/
-  elsif line=~/^%/
-  elsif line=~/\\end{document}/
-
+while (line = gets)
+  line.gsub!(/\\diff/, '\mathrm{d}')
+  case line
+  when /\\section\{(.*)\}/
+    puts "# #{$1}"
+  when /\\subsection\{(.*)\}/
+    puts "## #{$1}"
+  when /\\subsubsection\{(.*)\}/
+    puts "### #{$1}"
+  when /\\ans\{(.*)\}\{(.*)\}/
+    puts "### #{$1}. (#{$2})"
+  when /\\begin\{equation\}/
+    puts "\n$$"
+  when /\\end\{equation\}/
+    puts "$$\n\n"
+  when /^%/
+    # nop
   else
-    puts line.chomp
+    puts line
   end
 end
